c15095327c1b96d47ae1f1c93bfbd95d860da6e4
Jonas Schaarup <jonasfrschaarup@gmail.com>
2025-06-12 10:47:49 +0200

    Fintunig secondary care

--- make_thesis.R
+++ make_thesis.R
@@ -7,8 +7,6 @@ quarto_render(output_format = "html")
 
 
 #pdf
-#install.packages("tinytex")
-#tinytex::install_tinytex() install tinytex for processing LaTeX files
 
 quarto_render(output_format = "pdf")
 
